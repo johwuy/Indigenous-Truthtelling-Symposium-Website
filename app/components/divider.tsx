8c886ab32c44886ed9a7321ef4623b817ac6698d
Johnny Wu <johwuy@gmail.com>
2025-11-14 03:28:28 +0000

    Update divider to be full width

--- app/components/divider.tsx
+++ app/components/divider.tsx
@@ -5,9 +5,9 @@ interface DividerProps {
 
 function Divider({color1, color2}: DividerProps) {
   if (color2 == undefined) {
-    return <div className="h-4" style={{backgroundColor: color1}}></div>
+    return <div className="h-4 w-full" style={{backgroundColor: color1}}></div>
   }
-  return <><div className="h-2" style={{backgroundColor: color1}}></div><div className="h-2" style={{backgroundColor: color2}}></div></>
+  return <><div className="h-2 w-full" style={{backgroundColor: color1}}></div><div className="h-2" style={{backgroundColor: color2}}></div></>
 }
 
 export default Divider;
