23615247bebb799b18cbebb6df9c51e2d3d56077
Johnny Wu <johwuy@gmail.com>
2025-11-16 19:22:11 +0000

    Remove button container and add styling

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,14 +1,12 @@
 type ButtonProps = {
   borderColor: string;
-  text: string;        
+  text: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ borderColor, text}) => {
+const Button: React.FC<ButtonProps> = ({ borderColor, text }) => {
   return (
-    <div className ="flex w-1/2 h-screen items-center justify-center"> 
-        <div className={`inline-block text-center p-4 border-4 rounded-full w-sm:w-1/8 text-xl sm:text-2xl ${borderColor} bg-black-200`}>
-            {text}
-        </div>
+    <div className={`text-white italic inline-block text-center p-4 border-4 rounded-full w-sm:w-1/8 text-xl sm:text-2xl ${borderColor} bg-black-200`}>
+      {text}
     </div>
   );
 };
