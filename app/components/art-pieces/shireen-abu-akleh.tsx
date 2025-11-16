b52379cbe19175ccb5e05cc8dfffcf3027ca466d
Johnny Wu <johwuy@gmail.com>
2025-11-16 19:41:05 +0000

    Add image for Shireen

--- app/components/art-pieces/shireen-abu-akleh.tsx
+++ app/components/art-pieces/shireen-abu-akleh.tsx
@@ -8,8 +8,8 @@ function ShireenAbuAkleh() {
         <p className="text-center text-3xl/relaxed text-creme mb-12">Before her horrific murder at the hands of the Israeli military, Shireen was among Palestine’s greatest advocates and storytellers. Palestinian journalists show unmatched courage in confronting a genocidal army to give voice to that military’s victims. Shireen’s image is made up of the other 244 + Palestinian journalists who have lost their lives in the genocide.</p>
       <DownloadImage fileName="MainBefore.jpg" downloadLink="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg">
         <Image
-          src="/image3.png"
-          alt="Poppies in Palestine Series by Monah Kadah"
+          src="/shireen1.png"
+          alt="Shireen Abu Akleh"
           width={727}
           height={549}
         />
