9eb61341f619e2d4822846aaf5c74aa26ace23c6
Johnny Wu <johwuy@gmail.com>
2025-11-16 02:43:44 +0000

    Add pins to edu pieces

--- app/edu-pieces/page.tsx
+++ app/edu-pieces/page.tsx
@@ -1,6 +1,7 @@
 import BisanOwda from "../components/art-pieces/bisan-owda";
 import RefaatAlAreer from "../components/art-pieces/refaat-al-areer";
 import ShireenAbuAkleh from "../components/art-pieces/shireen-abu-akleh";
+import Pin from "../components/pin";
 
 function EduPieces() {
   return (
@@ -9,6 +10,15 @@ function EduPieces() {
       <BisanOwda />
       <RefaatAlAreer />
       <ShireenAbuAkleh />
+      <Pin color="#7B9F49">
+        <span className="font-bold">Project Our Art:</span> Thousands of Artists and art lovers share their most moving pieces with the use of projectors. Feel free to project our art on the side of a building, on campus, in your own living room or wherever else you see fit!
+      </Pin>
+      <Pin color="#F37E6A" shift="right" className="self-end">
+        <span className="font-bold">Project Our Art:</span> Click the Link below to see a list of cost effective options to print our Art pieces on 5x5, 7x8, or 8x10 foot canvas that include a metal stand. 
+      </Pin>
+      <Pin color="#F1BE4B">
+        For financial assistance for you and your organization, click the link below to see if you qualify to receive a print of our art: 
+      </Pin>
     </div>
   );
 }
