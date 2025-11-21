6092f1073ca20d435829cd7e85244aa764fa1cbf
Johnny Wu <johwuy@gmail.com>
2025-11-21 00:39:11 +0000

    Replace footer image

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,10 +5,10 @@ function Footer() {
   return (
     <footer>
       <Image
-        src="/image2.webp"
+        src="/footer.png"
         alt="Blossoming Under Seige by Mona Kadah"
-        width={1544}
-        height={1166}
+        width={1564}
+        height={1126}
         className="w-full h-auto opacity-34"
       />
       <Divider color1="#F37E6A" color2="#F1BE4B"></Divider>
